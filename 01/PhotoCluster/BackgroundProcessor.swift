0738ea01306a988bcf2380ae7765c31bd1e75048
Dylan Marriott <dmarriott@evernote.com>
2017-02-04 21:57:06 +0100

    Improved background loading

--- 01/PhotoCluster/BackgroundProcessor.swift
+++ 01/PhotoCluster/BackgroundProcessor.swift
@@ -24,18 +24,24 @@ class BackgroundProcessor: NSObject {
       return
     }
     processing = true
-    self.process()
+    print("Starting bg processing", Date())
+    DispatchQueue.global().async {
+      self.process()
+    }
   }
 
-  @objc private func process() {
+  private func process() {
     let realm = try! Realm()
     let photos = realm.objects(Photo.self).filter { $0.metaData == nil }
     if photos.isEmpty {
+      print("Processed all photos", Date())
       processing = false
       return
     }
     self.loadMetaData(photo: photos.first!) {
-      self.process()
+      DispatchQueue.global().async {
+        self.process()
+      }
     }
   }
 
@@ -54,49 +60,58 @@ class BackgroundProcessor: NSObject {
 
   private func loadThumbImage(photo: Photo, metaData: PhotoMetaData, completion:@escaping (Void)->(Void)) {
     let asset = PHAsset.fetchAssets(withLocalIdentifiers: [photo.assetId], options: nil).firstObject!
-    photo.getAssetThumbnail(asset: asset, size: 320, cache: false, completion: { (_image: UIImage?) -> (Void) in
-      if let image = _image {
-        let avgColor = image.areaAverage()
-        let comps = avgColor.cgColor.components!
-        metaData.red = Double(comps[0])
-        metaData.green = Double(comps[1])
-        metaData.blue = Double(comps[2])
+    let manager = PHImageManager.default()
+    let options = PHImageRequestOptions()
+    options.isSynchronous = true
+    options.deliveryMode = .highQualityFormat
+
+    let _ = autoreleasepool {
+      manager.requestImage(for: asset, targetSize: CGSize(width: 480, height: 480), contentMode: .aspectFit, options: options, resultHandler: {(_image, info)->Void in
+        if let image = _image {
+          let avgColor = image.areaAverage()
+          let comps = avgColor.cgColor.components!
+          metaData.red = Double(comps[0])
+          metaData.green = Double(comps[1])
+          metaData.blue = Double(comps[2])
 
 
-        var hue : CGFloat = 0
-        var saturation : CGFloat = 0
-        var brightness : CGFloat = 0
-        var alpha : CGFloat = 0
-        if avgColor.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha) {
-          metaData.hue = Double(hue)
-          metaData.saturation = Double(saturation)
-          metaData.brightness = Double(brightness)
+          var hue : CGFloat = 0
+          var saturation : CGFloat = 0
+          var brightness : CGFloat = 0
+          var alpha : CGFloat = 0
+          if avgColor.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha) {
+            metaData.hue = Double(hue)
+            metaData.saturation = Double(saturation)
+            metaData.brightness = Double(brightness)
+          }
         }
-      }
-      completion()
-    })
+        completion()
+      })
+    }
   }
 
   private func loadFullImage(photo: Photo, metaData: PhotoMetaData, completion:@escaping (Void)->(Void)) {
     let asset = PHAsset.fetchAssets(withLocalIdentifiers: [photo.assetId], options: nil).firstObject!
     let manager = PHImageManager.default()
     let options = PHImageRequestOptions()
-    options.isSynchronous = false
+    options.isSynchronous = true
     options.deliveryMode = .highQualityFormat
 
-    manager.requestImageData(for: asset, options: options) { (_data, uti, orientation, info) in
-      if let data = _data {
-        let image = CIImage(data: data)!
-        let options = [CIDetectorAccuracy: CIDetectorAccuracyLow]
-        let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: options)!
-        let faces = faceDetector.features(in: image)
-        metaData.faces = faces.count
+    let _ = autoreleasepool {
+      manager.requestImageData(for: asset, options: options) { (_data, uti, orientation, info) in
+        if let data = _data {
+            let image = CIImage(data: data)!
+            let options = [CIDetectorAccuracy: CIDetectorAccuracyLow]
+            let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: options)!
+            let faces = faceDetector.features(in: image)
+            metaData.faces = faces.count
 
-        let textDetector = CIDetector(ofType: CIDetectorTypeText, context: nil, options: options)!
-        let texts = textDetector.features(in: image)
-        metaData.texts = texts.count
+            let textDetector = CIDetector(ofType: CIDetectorTypeText, context: nil, options: options)!
+            let texts = textDetector.features(in: image)
+            metaData.texts = texts.count
+        }
+        completion()
       }
-      completion()
     }
   }
 }
